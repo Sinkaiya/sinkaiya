087f53ed75c448656446a51113b008f25530bc7a
Sinkaiya <sinkaiya@gmail.com>
2017-10-25 09:58:16 +0300

    Post list page and post detail page

---
+++ blogapp/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+# Здесь будет модель для теста Айзенка
+# class Eysenck(models.Model):
+#     firstname = models.CharField(max_length=255)
+#     lastname = models.CharField(max_length=255)
+#     surname = models.CharField(max_length=255)
+#     age = models.IntegerField()
+#     versy = models.IntegerField()
+#     vert = models.CharField(max_length=255)
+#     neurotism = models.IntegerField()
+#     neurotic = models.CharField(max_length=255)
+#     unfair = models.IntegerField()
+#     fairman = models.CharField(max_length=255)
