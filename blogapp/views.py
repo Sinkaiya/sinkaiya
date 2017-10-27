1879cd6fa19068977b474538af16471bacf39647
Sinkaiya <sinkaiya@gmail.com>
2017-10-27 14:18:59 +0300

    404 page added

--- blogapp/views.py
+++ blogapp/views.py
@@ -7,6 +7,11 @@ def post_list(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'blogapp/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blogapp/post_detail.html', {'post': post})
+
+
+def not_found_view(request):
+    return render(request, 'blogapp/404.html', {})
