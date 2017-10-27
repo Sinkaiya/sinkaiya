47d7d8737ee4cda9dc9f3c4abe6decd084b20dab
Sinkaiya <sinkaiya@gmail.com>
2017-10-27 14:54:51 +0300

    Contacts page added

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^notfound$', views.not_found_view, name='notfound')
+    url(r'^notfound$', views.not_found_view, name='notfound'),
+    url(r'^contacts', views.contacts, name='contacts'),
 ]
